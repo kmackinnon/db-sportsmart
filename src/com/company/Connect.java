a6396c2bb8e58b9d13a649f38adef80367244221
kmackinnon <keith.mackinnon@mail.mcgill.ca>
2016-03-12 14:19:40 -0500

    Connect is now a utility interface that can be used by calling openConnection

--- src/com/company/Connect.java
+++ src/com/company/Connect.java
@@ -6,45 +6,22 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
 
 public class Connect {
-
 	static final String JDBC_DRIVER = "org.postgresql.Driver";
 
-	public static void main(String args[]) throws ClassNotFoundException {
-		Class.forName(JDBC_DRIVER);
-		Creds creds = new Creds("creds.txt");
-
-		Connection db;
-		try {
-			db = DriverManager.getConnection(creds.getDbUrl(), creds.getUser(), creds.getPass());
-			testQuery(db);
-			db.close();
-		} catch (SQLException e) {
-			e.printStackTrace();
-			System.out.println("DB connection problems");
-		}
-	}
-
 	/**
-	 * Just for testing purposes
-	 * @param db
+	 * Use try with resources in order to ensure connection gets closed each time
+	 * 
+	 * @return
+	 * @throws ClassNotFoundException
 	 * @throws SQLException
 	 */
-	private static void testQuery(Connection db) throws SQLException {
-		// Test query execution
-		Statement stmt = db.createStatement();
-		ResultSet rs = stmt.executeQuery("SELECT * FROM Item");
-
-		while (rs.next()) {
-			System.out.println(rs.getString("item_name"));
-		}
-
-		rs.close();
-		stmt.close();
+	public static Connection openConnection() throws ClassNotFoundException, SQLException {
+		Class.forName(JDBC_DRIVER);
+		Creds creds = new Creds("creds.txt");
+		return DriverManager.getConnection(creds.getDbUrl(), creds.getUser(), creds.getPass());
 	}
 
 	private static class Creds {
@@ -53,10 +30,7 @@ public class Connect {
 		private String pass;
 
 		/**
-		 * File structure: 
-		 * database 
-		 * url 
-		 * user pass
+		 * File structure: database url user pass
 		 * 
 		 * @param fileName
 		 */
@@ -74,18 +48,23 @@ public class Connect {
 		public String getUser() {
 			return user;
 		}
+
 		public void setUser(String user) {
 			this.user = user;
 		}
+
 		public String getPass() {
 			return pass;
 		}
+
 		public void setPass(String pass) {
 			this.pass = pass;
 		}
+
 		public String getDbUrl() {
 			return dbUrl;
 		}
+
 		public void setDbUrl(String dbUrl) {
 			this.dbUrl = dbUrl;
 		}
